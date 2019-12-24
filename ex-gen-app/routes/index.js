011761fc4d2fa7d4137f51772fd8392fcb6831a9
遠藤達也 <kmno4hno3@ezweb.ne.jp>
2019-12-25 00:06:11 +0900

    comment add end

--- ex-gen-app/routes/index.js
+++ ex-gen-app/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();    //Router:ルーティングに関する機能をまとめたもの
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {      //router.get(アドレス, 関数)
   res.render('index', { title: 'Express' });
 });
 
