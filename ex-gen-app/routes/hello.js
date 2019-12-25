3e83acb742ac27eb939a8f500eb23f155c009b3e
kmno4hno3 <kmno4hno3@ezweb.ne.jp>
2019-12-25 15:44:17 +0900

    form end

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -2,14 +2,20 @@ var express = require("express");
 var router = express.Router();
 
 router.get('/', (req, res, next) => {      //ここはhello下の「/」になる(/hello/)
-    var name = req.query.name;      //クエリーパラメーターを送る
-    var mail = req.query.mail;      //クエリーパラメーターを送る
     var data = {
         title: "Hello!",
-        content: "あなたの名前は、" + name + '。<br>' +
-            'メールアドレスは、' + mail + 'です。'
+        content: '※何か書いて送信して下さい。'
     };
     res.render("hello", data);
 })
+
+router.post('/post', (req, res, next) => {    //postメソッドでPOSTアクセスの処理を行う
+    var msg = req.body['message'];
+    var data = {
+        title: 'Hello!',
+        content: 'あなたは、「' + msg + '」と送信しました。'
+    };
+    res.render('hello', data);
+});
 
 module.exports = router;
