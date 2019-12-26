506386e38d8718f9d64ae9f9306dfabdb1bb7639
遠藤達也 <kmno4hno3@ezweb.ne.jp>
2019-12-26 18:00:50 +0900

    sql end

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -1,31 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');       //MySQLモジュールのロード
 
-//モジュールのロードは基本的にapp.jsだが、以下の機能は/helloでしか使わないのでここでhello.jsでロードする
-var http = require('https');                            //webサイトへのアクセスするためのモジュール(ここではHTTPSを使う)
-var parseString = require('xml2js').parseString;
+var mysql_setting =  {              //MySQLにアクセスする際に必要となる設定情報を変数にまとめている
+    host        : 'localhost',         //MySQLサーバーがあるホスト名
+    user        : 'root',              //DBへのアクセスに用いるユーザー名(デフォルト)
+    password    : '',                  //アクセス時に用いるパスワード(デフォルト)
+    database    : 'my-nodeapp-db'      //利用するDB名
+};
 
 router.get('/', (req, res, next) => {      //ここはhello下の「/」になる(/hello/)
-    var opt = {
-        host: 'news.google.com',           //ホスト(ドメイン)
-        port: 443,                         //ポート番号(HTTPSアクセスなら443番、HTTPアクセスなら80番)
-        path: '/rss?ie=UTF-8&oe=UTF-8&hl=en-US&gl=US&ceid=US:en'        //パス(ドメイン以降の部分)
-    };
-    http.get(opt, (res2) => {               //http.get(アクセスに関するオプション設定, コールバック関数)、第1引数の情報を元にアクセス開始し、GET終了時に第2引数のコールバック関数を呼び出す
-        var body = '';                      //https.getのコールバック関数ではresponseオブジェクトが引数に渡される
-        res2.on('data', (data) => {         //このresponseにデータを取得した際のイベントを設定
-            body += data;
-        });
-        res2.on('end', () => {
-            parseString(body.trim(), (err, result) => {     //XMLデータをパースする(XMLテキストを元にXMLのオブジェクトを作る)、parseString(XMLのテキスト, コールバック関数)
-                var data = {                                //コールバック関数の第1引数:エラーに関するオブジェクト、第2引数:パースして作成されたオブジェクト
-                    title: 'Hello!',
-                    content: result.rss.channel[0].item     //RSSデータから必要なデータを取り出している
-                };
+    
+    //コネクションの用意
+    var connection = mysql.createConnection(mysql_setting);     //コネクションを用意(DB接続を管理するオブジェクト、引数にはDBに関する設定情報を指定)
+
+    //データベースに接続
+    connection.connect();               //connectメソッド:コネクションで指定したDBサーバーに接続を開始
+
+    //データを取り出す
+    connection.query('SELECT * from mydata',          //queryメソッド:「クエリー」という命令(SQL言語)をDBサーバーに送信し、結果を受け取る
+            function (error, results, fields) {       //error:発生したエラー情報をまとめたもの、results:クエリー実行して得られた値、fields:取り出したフィールドに関する情報をまとめたもの
+            //データベースアクセス完了時の処理
+            if(error == null) {
+                var data = {title: 'mysql', content: results};
                 res.render('hello', data);
-            });
-        })
+            }
     });
+
+    //接続を解除
+    connection.end();       //DBアクセスを終了
 });
 
 module.exports = router;
